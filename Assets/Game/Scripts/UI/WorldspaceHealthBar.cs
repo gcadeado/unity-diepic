86f1ea916ec25b22c353a51c8a1b44b1e501686c
Gabriel Cencic <gabriel.cencic@gmail.com>
2021-10-03 17:13:22 -0300

    Add health bar UI

---
+++ Assets/Game/Scripts/UI/WorldspaceHealthBar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Health))]
+public class WorldspaceHealthBar : MonoBehaviour
+{
+    [Tooltip("Image component displaying health left")]
+    public Image healthBarImage;
+    [Tooltip("The floating healthbar pivot transform")]
+    public Transform healthBarPivot;
+    [Tooltip("Whether the health bar is visible when at full health or not")]
+    public bool hideFullHealthBar = true;
+
+    private Health m_health;
+
+    void Awake()
+    {
+        m_health = GetComponent<Health>();
+        DebugUtility
+            .HandleErrorIfNullGetComponent
+            <Health, WorldspaceHealthBar>(m_health,
+            this,
+            gameObject);
+    }
+
+    void Update()
+    {
+        // update health bar value
+        healthBarImage.fillAmount = m_health.currentHealth / m_health.maxHealth;
+
+        // hide health bar if needed
+        if (hideFullHealthBar)
+            healthBarPivot.gameObject.SetActive(healthBarImage.fillAmount != 1);
+    }
+}
